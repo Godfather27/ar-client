f9a179da09104764deafc4fc7d302a00e0e852ef
Alexander Gabriel <agabriel.mmt-b2014@fh-salzburg.ac.at>
2018-06-24 19:24:39 +0200

    update for mac

--- src/webrtc.js
+++ src/webrtc.js
@@ -4,32 +4,34 @@ function initializeWebRTC(video, out){
   var gl = video.getContext();       //get webGl context
   var canvas = gl.canvas;               //get gl's canvas
 
-
+  console.log("HALLO")
   var video = document.querySelector('#localVideo');
   var stream = canvas.captureStream();
   video.srcObject = stream;
   video.muted = true;
   video.play();
-  // webrtc = new SimpleWebRTC({
-  //   localVideoEl: video.domElement,
-  //   remoteVideosEl: out,
-  //   autoRequestMedia: true,
-  //   enableDataChannels: true,
-  // });
+  webrtc = new SimpleWebRTC({
+    localVideoEl: video,
+    remoteVideosEl: out,
+    autoRequestMedia: true,
+    enableDataChannels: true,
+  });
 
-  // webrtc.on('channelMessage', (peer, channel, data) => {
-  //   if (channel === 'chat') {
-  //     console.log(data.type, data.payload);
-  //   }
-  // });
+  webrtc.on('channelMessage', (peer, channel, data) => {
+    if (channel === 'chat') {
+      console.log(data.type, data.payload);
+    }
+  });
 
-  // webrtc.on('readyToCall', () => {
-  //   webrtc.joinRoom('lexi', () => {
-  //     webrtc.sendDirectlyToAll('chat', 'message', "ar joined the party");
-  //   });
-  // });
+  webrtc.on('readyToCall', () => {
+    console.log("READY")
+    webrtc.joinRoom('lexi', () => {
+      console.log("JOIN")
+      webrtc.sendDirectlyToAll('chat', 'message', "ar joined the party");
+    });
+  });
 
-  // return webrtc;
+  return webrtc;
 }
 
 export {initializeWebRTC};
